ba7efb8026d97dbf0cfaa1bd28dfe87eefdd9434
Luca Bondi <bondi.luca@gmail.com>
2019-03-22 12:35:05 +0100

    integrated libraries and name change to build a bundled app

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -21,14 +21,12 @@
 #include <juce_audio_processors/juce_audio_processors.h>
 #include <juce_audio_utils/juce_audio_utils.h>
 #include <juce_core/juce_core.h>
-#include <juce_cryptography/juce_cryptography.h>
 #include <juce_data_structures/juce_data_structures.h>
 #include <juce_dsp/juce_dsp.h>
 #include <juce_events/juce_events.h>
 #include <juce_graphics/juce_graphics.h>
 #include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_gui_extra/juce_gui_extra.h>
-#include <juce_opengl/juce_opengl.h>
 
 
 #if ! DONT_SET_USING_JUCE_NAMESPACE
@@ -40,7 +38,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "juce-beam";
+    const char* const  projectName    = "eBeamer";
     const char* const  companyName    = "ISPL Polimi";
     const char* const  versionString  = "0.3";
     const int          versionNumber  = 0x300;
