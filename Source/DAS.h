7071c47022f864972a1b34911fea312dd794c36d
Luca Bondi <luca.bondi@polimi.it>
2020-04-15 09:50:49 +0200

    Added DAS

---
+++ Source/DAS.h
@@ -0,0 +1,30 @@
+/*
+  ==============================================================================
+
+    DAS.h
+    Created: 15 Apr 2020 9:21:10am
+    Author:  Luca Bondi
+
+  ==============================================================================
+*/
+
+#pragma once
+#include Eigen
+
+namespace DAS{
+
+/** Farfield Linear Microphone Array Delay and Sum beamformer
+ 
+ This class is used do setup a LMA and compute the FIR impulse response
+ */
+class FarfieldLMA{
+    
+public:
+    FarfieldLMA();
+    ~FarfieldLMA();
+    
+private:
+    
+};
+
+}
