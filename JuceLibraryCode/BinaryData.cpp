b03f1b7557b3988192219c67b8372bc291de042f
Luca Bondi <bondi.luca@gmail.com>
2022-02-19 11:02:03 -0500

    juce version update

--- JuceLibraryCode/BinaryData.cpp
+++ JuceLibraryCode/BinaryData.cpp
@@ -6660,6 +6660,7 @@ static const unsigned char temp_binary_data_30[] =
 const char* UmfPackSupport = (const char*) temp_binary_data_30;
 
 
+const char* getNamedResource (const char* resourceNameUTF8, int& numBytes);
 const char* getNamedResource (const char* resourceNameUTF8, int& numBytes)
 {
     unsigned int hash = 0;
@@ -6778,6 +6779,7 @@ const char* originalFilenames[] =
     "UmfPackSupport"
 };
 
+const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
 const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8)
 {
     for (unsigned int i = 0; i < (sizeof (namedResourceList) / sizeof (namedResourceList[0])); ++i)
